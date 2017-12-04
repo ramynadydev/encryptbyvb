6e336acbc2dd814a8341dccc5bf660b71c15ae8b
ramynadydev <34231863+ramynadydev@users.noreply.github.com>
2017-12-04 10:29:13 +0200

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("enter your string to encrypt: ")
+        Dim text As String = Console.ReadLine()
+        Console.WriteLine(" string after encryption : " + util.Encrypt(text))
+        Console.WriteLine("enter your string to decrypt: ")
+        Dim texttodecrypt As String = Console.ReadLine()
+        Console.WriteLine(" string after decryption : " + util.Decrypt(texttodecrypt))
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ util.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Public Class util
+    Private Shared PASSWORD_KEY As String = "test"
+
+    Private Shared rawSecretKey As Byte() = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+
+    Public Shared Function Encrypt(ByVal dencryptedBase64 As String) As String
+        Dim passwordKey As Byte() = encodeDigest(PASSWORD_KEY)
+        Dim rijndael As RijndaelManaged = New RijndaelManaged()
+        Dim encryptorTransform As ICryptoTransform = rijndael.CreateEncryptor(passwordKey, rawSecretKey)
+        Dim plainTextBytes As Byte() = Encoding.ASCII.GetBytes(dencryptedBase64)
+        Dim newClearData As Byte() = encryptorTransform.TransformFinalBlock(plainTextBytes, 0, plainTextBytes.Length)
+        Return Convert.ToBase64String(newClearData)
+    End Function
+    Public Shared Function Decrypt(ByVal encryptedBase64 As String) As String
+        Dim passwordKey As Byte() = encodeDigest(PASSWORD_KEY)
+        Dim rijndael As RijndaelManaged = New RijndaelManaged()
+        Dim rijndaelDecryptor As ICryptoTransform = rijndael.CreateDecryptor(passwordKey, rawSecretKey)
+        Dim newClearData As Byte() = rijndaelDecryptor.TransformFinalBlock(Convert.FromBase64String(encryptedBase64), 0, Convert.FromBase64String(encryptedBase64).Length)
+        Return Encoding.ASCII.GetString(newClearData)
+    End Function
+
+    Private Shared Function encodeDigest(ByVal text As String) As Byte()
+        Dim x As MD5CryptoServiceProvider = New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim data As Byte() = Encoding.ASCII.GetBytes(text)
+        Return x.ComputeHash(data)
+    End Function
+End Class
